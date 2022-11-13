dc1e72caea2a9de17821031f9d82a170b37e8bbe
alex <arevaloalex9@gmail.com>
2022-11-13 11:27:26 -0500

    Add docker-compose to local.yml

--- ansible-run.sh
+++ ansible-run.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 
-## Install ansible
 sudo apt-add-repository -y ppa:ansible/ansible
 sudo apt-get update -y
 sudo apt-get install -y curl git software-properties-common ansible
-
-## pull ansible
 sudo ansible-pull -U https://github.com/alexarevalo9/ansible-setup-tools.git
