c3c6bed6b6fc78d3b67edb3feea924172038165e
alex <arevaloalex9@gmail.com>
2022-12-19 20:45:30 -0500

    Fix ssh

--- ansible-ssh.sh
+++ ansible-ssh.sh
@@ -4,4 +4,4 @@ sudo apt-add-repository -y ppa:ansible/ansible
 sudo apt-get update -y
 sudo apt-get install -y curl git software-properties-common ansible
 git clone https://github.com/alexarevalo9/ansible-setup-tools.git
-sudo ansible-playbook --ask-become-pass --ask-vault-pass ansible-setup-tools/local.yml -t ssh
+ansible-playbook --ask-become-pass --ask-vault-pass ansible-setup-tools/local.yml -t ssh
