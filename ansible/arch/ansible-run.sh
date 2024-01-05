e9663d2cccd6979c0c039a6d25b91cb18373bac4
alex <arevaloalex9@gmail.com>
2024-01-05 10:26:53 -0500

    fix: ansible arch script

--- ansible/arch/ansible-run.sh
+++ ansible/arch/ansible-run.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-// TODO: Install yay
-sudo pacman -Syyu --noconfirm ansible
-sudo pacman -S --noconfirm curl git base-devel
+
+RUN pacman -Sy --noconfirm && pacman -S --noconfirm ansible curl git base-devel
 git clone https://github.com/alexarevalo9/ansible-setup-tools.git
 sudo ansible-playbook --ask-become-pass --ask-vault-pass ansible-setup-tools/ansible/arch/local.yml -vvv
