68d544bd04cd003af0247e7418b6fdaa32f00ba1
alex <arevaloalex9@gmail.com>
2024-01-05 12:28:06 -0500

    fix: snap task

--- ansible/arch/ansible-run.sh
+++ ansible/arch/ansible-run.sh
@@ -3,4 +3,4 @@
 sudo pacman -Sy --noconfirm ansible curl git base-devel
 git clone https://github.com/alexarevalo9/ansible-setup-tools.git
 sudo cp ~/ansible-setup-tools/ansible/arch/local.yml ~/ansible-setup-tools/local.yml
-sudo ansible-playbook --ask-become-pass --ask-vault-pass ansible-setup-tools/local.yml -vvv
+ansible-playbook --ask-become-pass --ask-vault-pass ansible-setup-tools/local.yml -vvv
