ea8ff065700892a11f18e5de1f5c070fc032d6a3
alex <arevaloalex9@gmail.com>
2024-01-13 15:58:56 -0500

    fix: dotfiles script

--- scripts/dotfiles.sh
+++ scripts/dotfiles.sh
@@ -4,15 +4,19 @@ DIR_1=$1
 DIR_2=$2
 
 function config {
-   /usr/bin/git --git-dir=$DIR_1 --work-tree=$DIR_2 $@
+  /usr/bin/git --git-dir=$DIR_1 --work-tree=$DIR_2 $@
 }
+
 mkdir -p .config-backup
+
 config checkout
+
 if [ $? = 0 ]; then
-  echo "Checked out config.";
-  else
-    echo "Backing up pre-existing dot files.";
-    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
-fi;
+  echo "Checked out config."
+else
+  echo "Backing up pre-existing dot files."
+  config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} bash -c 'mkdir -p .config-backup/$(dirname {}) && mv {} .config-backup/{}'
+fi
+
 config checkout
 config config status.showUntrackedFiles no
