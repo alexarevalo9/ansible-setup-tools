0554f3937bdcbe8b75b7585825c6096969bdeaaf
alex <arevaloalex9@gmail.com>
2024-01-22 10:32:28 -0500

    fix: update ansible-run.sh and local.yml files, remove Anki task, and modify gpg.yml

--- ansible/debian/ansible-run.sh
+++ ansible/debian/ansible-run.sh
@@ -4,4 +4,5 @@ sudo apt-add-repository -y ppa:ansible/ansible
 sudo apt-get update -y
 sudo apt-get install -y curl git software-properties-common ansible
 git clone https://github.com/alexarevalo9/ansible-setup-tools.git
-sudo ansible-playbook --ask-become-pass --ask-vault-pass ansible-setup-tools/ansible/debian/local.yml -vvv
+sudo cp ~/ansible-setup-tools/ansible/debian/local.yml ~/ansible-setup-tools/local.yml
+sudo ansible-playbook --ask-become-pass --ask-vault-pass ansible-setup-tools/local.yml -vvv
